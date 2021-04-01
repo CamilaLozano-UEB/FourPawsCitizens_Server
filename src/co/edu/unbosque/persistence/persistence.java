8e8c26eca209cdd4560ad773792f0a6c566d545b
JuanaTorres <jvtorresp@unbosque.edu.co>
2021-04-01 15:52:48 -0500

    Method of writeCSV

--- src/co/edu/unbosque/persistence/persistence.java
+++ src/co/edu/unbosque/persistence/persistence.java
@@ -1,5 +1,29 @@
 package co.edu.unbosque.persistence;
 
-public class persistence {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class persistence {
+	private String rute = "./Data/pets-case.csv";
+
+	public int writeCSV(String dato) {
+
+		File f = new File(this.rute); 
+
+		try {
+			FileWriter fw = new FileWriter(f); 
+			PrintWriter pw = new PrintWriter(fw); 
+
+			pw.println(dato); 
+
+			fw.close();
+
+		} catch (IOException e) {
+			return -1;
+		}
+
+		return 0;
+	}
 }
