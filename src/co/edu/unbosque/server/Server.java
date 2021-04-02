e5d4bb16b4a800eb01d2acafb0e175c016a98548
Nicolas-Pena-Mogollon <nicop4643@gmail.com>
2021-04-01 19:05:44 -0500

    Spelling error corrected

--- src/co/edu/unbosque/server/Server.java
+++ src/co/edu/unbosque/server/Server.java
@@ -47,12 +47,12 @@ public class Server {
 				// Keep requesting a name until we get a unique one.
 				String line = in.nextLine();
 				if (line.equals("Client")) {
-					out.println("Bienvenido a Ciudadanos de 4 patas" );
-					while(true) {
+					out.println("Bienvenido a Ciudadanos de 4 patas");
+					while (true) {
 						userIndex = clientWriters.size();
 						clientWriters.add(out);
-						out.println( " Ingrese el numero que desea"
-								+ " \n1. Crear un caso" + "\n2. Hablar con un agente");
+						out.println(
+								" Ingrese el numero que desea" + " \n1. Crear un caso" + "\n2. Hablar con un agente");
 						line = in.nextLine();
 						switch (line) {
 						case "1":
@@ -66,14 +66,14 @@ public class Server {
 							break;
 						}
 					}
-					
+
 				} else {
 					userIndex = agentesDisponibles.size();
 					agentesDisponibles.add(-1);
 					agentWriters.add(out);
 					out.println("Conectado como agente");
 					line = in.nextLine();
-					boolean chat=true;
+					boolean chat = true;
 					while (chat) {
 						if (line.equalsIgnoreCase("Si") || line.equalsIgnoreCase("Sí")) {
 							clientWriters.get(agentesDisponibles.get(userIndex)).println("Conexión establecida");
@@ -83,17 +83,19 @@ public class Server {
 								out.println("Me: " + line);
 							}
 						} else {
-							if((userIndex+1)==agentesDisponibles.size()) {
-								clientWriters.get(agentesDisponibles.get(userIndex)).println("No hay agentes disponibles");
-								chat=false;
-								line="exit";
-							}else {
-								clientWriters.get(agentesDisponibles.get(userIndex)).println("El agente "+(userIndex+1)+" a denegado su solicitud,\n escriba *esperar* si desea esperar de lo contrario escriba *exit*");
-							    agentesDisponibles.set(userIndex, -1);
-							    line="exit";
+							if ((userIndex + 1) == agentesDisponibles.size()) {
+								clientWriters.get(agentesDisponibles.get(userIndex))
+										.println("No hay agentes disponibles");
+								chat = false;
+								line = "exit";
+							} else {
+								clientWriters.get(agentesDisponibles.get(userIndex)).println("El agente "
+										+ (userIndex + 1)
+										+ " ha denegado su solicitud,\n escriba *esperar* si desea esperar de lo contrario escriba *exit*");
+								agentesDisponibles.set(userIndex, -1);
+								line = "exit";
 							}
-							
-							
+
 						}
 					}
 				}
@@ -112,7 +114,7 @@ public class Server {
 					if (agentesDisponibles.get(i) != -1) {
 						while (true) {
 							String input = in.nextLine();
-							if (input.toLowerCase().equals("exit")||input.toLowerCase().equals("esperar")) {
+							if (input.toLowerCase().equals("exit") || input.toLowerCase().equals("esperar")) {
 								break;
 							}
 							out.println("Me: " + input);
@@ -128,54 +130,51 @@ public class Server {
 			Persistence per = new Persistence();
 			// TODO Auto-generated method stub
 			out.println("Crear caso");
-			out.println("¿Que va a reportar? ingrese el numero\n"
-					+ "(1) Pérdida\n"
-					+ "(2) Robo\n"
-					+ "(3) Abandono\n"
-					+ "(4) Animal peligroso\n"
-					+ "(5) Manejo indebido en vía pública. " );
-        	String op2 =(in.nextLine());
-        	out.println("Ingrese la especie");
-        	String specie =in.nextLine();
-        	out.println("Ingrese la Tamaño");
-        	String size =in.nextLine();
-        	out.println("Ingrese la Localidad");
-        	String neighborhood =in.nextLine();
-        	out.println("Ingrese la Dirección");
-        	String address =in.nextLine();
-        	out.println("Ingrese su nombre");
-        	String name =in.nextLine();
-        	out.println("Ingrese su Telefono");
-        	String phone =in.nextLine();
-        	out.println("Ingrese su correo");
-        	String email =in.nextLine();
-        	out.println("Ingrese su comentarios");
-        	String comment =in.nextLine();
-        	String event;
-        	switch (op2) {
+			out.println("¿Que va a reportar? ingrese el numero\n" + "(1) Pérdida\n" + "(2) Robo\n" + "(3) Abandono\n"
+					+ "(4) Animal peligroso\n" + "(5) Manejo indebido en vía pública. ");
+			String op2 = (in.nextLine());
+			out.println("Ingrese la especie");
+			String specie = in.nextLine();
+			out.println("Ingrese la Tamaño");
+			String size = in.nextLine();
+			out.println("Ingrese la Localidad");
+			String neighborhood = in.nextLine();
+			out.println("Ingrese la Dirección");
+			String address = in.nextLine();
+			out.println("Ingrese su nombre");
+			String name = in.nextLine();
+			out.println("Ingrese su Telefono");
+			String phone = in.nextLine();
+			out.println("Ingrese su correo");
+			String email = in.nextLine();
+			out.println("Ingrese su comentarios");
+			String comment = in.nextLine();
+			String event;
+			switch (op2) {
 			case "1":
-				event="Pérdida";
+				event = "Pérdida";
 				break;
 			case "2":
-				event="Robo";
+				event = "Robo";
 				break;
 			case "3":
-				event="Abandono";
+				event = "Abandono";
 				break;
 			case "4":
-				event="Animal peligroso";
+				event = "Animal peligroso";
 				break;
 			case "5":
-				event="Manejo indebido en vía pública";
+				event = "Manejo indebido en vía pública";
 				break;
-			
+
 			default:
-				event="No se especifico";
+				event = "No se especifico";
 				break;
 			}
-        	String report= event +";"+specie+";"+size+";"+ neighborhood+";"+address+";"+name+";"+phone+";"+email+";"+comment;
-        	out.println("El caso ha sido creado.");
-        	per.writeCSV(report);
+			String report = event + ";" + specie + ";" + size + ";" + neighborhood + ";" + address + ";" + name + ";"
+					+ phone + ";" + email + ";" + comment;
+			out.println("El caso ha sido creado.");
+			per.writeCSV(report);
 
 		}
 	}
